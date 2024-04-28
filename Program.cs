1dd162265bb1d336ca2a4770217fc53abbd86827
adakde <kuba-kasprzak12@wp.pl>
2024-04-28 18:06:35 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for(int i = 0; i < 100; i++) {
+    if(i %3 == 0 && i %5 == 0) {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(i %3 == 0) {
+        Console.WriteLine("Buzz");
+        }
+
+    else if(i %5 == 0){
+        Console.WriteLine("Fizz");
+    }
+    else {
+        Console.WriteLine(i);
+    }
+}
